9bf3061570213b93e3402fc071e741103efce2aa
Alabi Sodiq <adisco4420@gmail.com>
2020-09-08 19:25:31 +0100

    search api

---
+++ src/app/services/service.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ServiceService {
+  readonly apiKey = 'deokzgUjxm6QHQdp3H3aca1LSZcCpucc';
+  readonly baseUrl = 'https://api.giphy.com/v1';
+  constructor(private http: HttpClient) { }
+
+  search(q) {
+    const params = {api_key: this.apiKey, q, limit: '25', offset: '0', rating: 'Y', lang: 'en'};
+    return this.http.get(`${this.baseUrl}/gifs/search`, {params});
+  }
+}
